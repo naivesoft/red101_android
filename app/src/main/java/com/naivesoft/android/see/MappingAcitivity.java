f9250a3d595241967d7029f783d0ec838dad4157
DESKTOP-OAA8L15\admin <qiyi.wxc@gmail.com>
2017-12-17 23:43:57 +0800

    增加音符画面

--- app/src/main/java/com/naivesoft/android/see/MappingAcitivity.java
+++ app/src/main/java/com/naivesoft/android/see/MappingAcitivity.java
@@ -1,8 +1,15 @@
 package com.naivesoft.android.see;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+
+import com.naivesoft.android.see.widget.HeartLayout;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by admin on 2017/12/17.
@@ -10,10 +17,27 @@ import android.support.v4.app.FragmentActivity;
 
 public class MappingAcitivity extends FragmentActivity {
 
+    private Random mRandom = new Random();
+    private Timer mTimer = new Timer();
+    private HeartLayout mHeartLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapping);
+
+        mHeartLayout = findViewById(R.id.heart_layout);
+        mTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                mHeartLayout.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mHeartLayout.addHeart(randomColor());
+                    }
+                });
+            }
+        }, 500, 200);
     }
 
     @Override
@@ -21,8 +45,14 @@ public class MappingAcitivity extends FragmentActivity {
         super.onStart();
     }
 
+    private int randomColor() {
+        return Color.rgb(mRandom.nextInt(255), mRandom.nextInt(255), mRandom.nextInt(255));
+    }
+
     @Override
     protected void onDestroy() {
+        mTimer.cancel();
+
         super.onDestroy();
     }
 
